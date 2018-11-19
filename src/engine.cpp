51e8a9418e65db0f086f054e91edc135fd28144f
fcosta_oliveira <filipecosta90@gmail.com>
2018-11-19 13:49:03 +0000

    [add] setting up for configurations from env variables

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,12 +1,17 @@
 #include <cppkafka/cppkafka.h>
+#include <cstdlib>
+#include <iostream>
 
 using namespace std;
 using namespace cppkafka;
 
 int main() {
   // Create the config
+  const char* val = std::getenv("BROKER_LIST");
+  std::string broker_list = (val == NULL) ? std::string("127.0.0.1:9092") : std::string(val);
+  std::cout << "using broker_list " << broker_list << std::endl;
   Configuration config = {
-    { "metadata.broker.list", "127.0.0.1:9092" }
+    { "metadata.broker.list", broker_list }
   };
 
   // Create the producer
@@ -14,6 +19,6 @@ int main() {
 
   // Produce a message!
   string message = "hey there!";
-  producer.produce(MessageBuilder("my_topic").partition(0).payload(message));
+  producer.produce(MessageBuilder("test").partition(0).payload(message));
   producer.flush();
 }
